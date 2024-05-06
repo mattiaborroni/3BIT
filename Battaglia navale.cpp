68e243e8b2b7da2864548ce28b1a6e8ffa89585c
MATTIA BORRONI <153180606+mattiaborroni@users.noreply.github.com>
2024-05-06 23:02:39 +0200

    Sistemata bozza codice ma siamo lontani
    Bisogna ora sistemare procedura stampa campi

--- Battaglia navale.cpp
+++ Battaglia navale.cpp
@@ -30,24 +30,39 @@ void Scritta{
     cout<<"                          \/_/\/_/    \/_/\/_/    `\/__/     \/_/\/_/    \/___/     \/___/ "<<endl;
 }
 
+void Stampacampi {
+    
+    cout<<"*******************************************************\n";
+    cout<<"*****************   CAMPO 1  **************************\n";
+    cout<<"*******************************************************\n";
+    
+    cout<<"********************* \n\n";
+    cout<<"   0 1 2 3 4 5 6 7\n";
+    cout<<"---------------------\n";
+    
+    for (int x=0;x<8;x++) {
+        
+        cout<<x<<"| ";
+        
+        for(int y=0;y<8;y++){
+            cout<<campo1[x][y]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
 int main()
 {
-    
-//variabili
     srand(time(NULL));
     
-    int nave1oriz=rand()%10+1;
-    int nave1vert=rand()%10+1;
-    int nave2oriz=rand()%10+1;
-    int nave2vert=rand()%6+1;
-    int nave1aff=0;
-    int nave2aff=0;
+//variabili principali
     
-    char coord [9][9]={{"        "},{"        "},{"        "},{"        "},{"        "},{"        "},{"        "},{"        "}};
-    int oriz=0;
-    int vert=0;
-
-
+    char campo1 [10][10]={};
+    char campo2 [10][10]={};
+    
+   
+    
 //Cancella schermo
     clrscr();
 
@@ -59,64 +74,33 @@ int main()
   
     
     
-//mette la prima nave
+//mettere  le navi 
 
 
-coord [nave1oriz][nave1vert]=0 ;
-coord [nave1oriz+1][nave1vert]=0 ;
-coord [nave1oriz][nave1vert+1]=0 ;
-coord [nave1oriz+1][nave1vert+1]=0 ;
 
-//inserire la seconda nave senza toccare la prima
 
-while (coord[nave2oriz][nave2vert]==0 | coord[nave2oriz+1][nave2vert]==0 | coord[nave2oriz][nave2vert+1]==0 | coord[nave2oriz+1][nave2vert+1]==0){
-    nave2oriz=rand()%6+1;
-    nave2vert=rand()%6+1;
-    }
-
-//visualizza nave 2 sulla griglia
-coord [nave2oriz][nave2vert]=15 ;
-coord [nave2oriz+1][nave2vert]=15 ;
-coord [nave2oriz][nave2vert+1]=15 ;
-coord [nave2oriz+1][nave2vert+1]=15 ;
 
 //generatore del campo navale
 
-for (int t=0;t<8;t++){
 
-    cout<<"*******************************************************\n";
-    cout<<"Hai ancora "<<8-t<<" tentativi per vincere la partita\n";
-    cout<<"*******************************************************\n";
-    cout<<"   BATTAGLIA NAVALE \n\n";
-    cout<<"********************* \n\n";
-    cout<<"   0 1 2 3 4 5 6 7\n";
-    cout<<"---------------------\n";
+
     
-    for (int x=0;x<8;x++) {
-        
-        cout<<x<<"| ";
-        
-        for(int y=0;y<8;y++){
-            cout<<coord[x][y]<<" ";
-        }
-        cout<<endl;
-    }
  
 // inserimento coordinate
 
 do {
     cout<<"\nInserire coordinata orizzontale ";
     cin>>oriz;
-    if (oriz>7) {cout<<"valore fuori portata riprova.....\n";}
+    if (oriz>10) {cout<<"valore fuori portata riprova.....\n";}
 }
-while (oriz>7); 
+while (oriz>10); 
    
 do{
     cout<<"Inserire coordinata verticale ";
     cin>>vert;
-    if (vert>7){ cout<<"valore fuori portata riprova.....\n";}
+    if (vert>10){ cout<<"valore fuori portata riprova.....\n";}
     }
-while (vert>7); 
+while (vert>10); 
 
 
 if (coord[oriz][vert]==0) {
